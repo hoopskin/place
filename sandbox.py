969a8fd88b668753b6082a530cb266b5b3ef639c
Alex Sorenson <alex@DRCOX3.local>
2022-04-16 15:55:11 -0700

    Should now combine all GZIPs, prep data, and save to Pickle file.

--- sandbox.py
+++ sandbox.py
@@ -7,36 +7,46 @@ GZIP_FOLDER = "res/data/gzip-files"
 GZIP_FILE_LIST = [GZIP_FOLDER + "/" + f \
             for f in check_output(["ls", GZIP_FOLDER]).decode("utf8").split()]
 
+#list_of_dfs = [pd.read_csv(filename) for filename in GZIP_FILE_LIST]
+#for dataframe, filename in zip(list_of_dfs, GZIP_FILE_LIST)
+#    dataframe['filename'] = filename
+#combined_df = pd.concat(list_of_dfs, ignore_index=True)
+
+list_of_dfs = []
+total_size = 0
+
 for curFile in GZIP_FILE_LIST:
-    print(curFile)
+    curFileIdx = GZIP_FILE_LIST.index(curFile)
+    preLine = "%i / %i [%.2f%%]" % (curFileIdx, len(GZIP_FILE_LIST), (curFileIdx/len(GZIP_FILE_LIST))*100)
+    print(preLine + "\t"+ curFile)
 
-    print(curFile + "\tBuilding Init DF...")
-    place_data = pd.read_csv(curFile, compression='gzip',
-                                     header=0, sep=',', quotechar='"')
+    #Don't parse dates. It adds so much time and the format can be
+    #Easily manipulated as a string
+    print(preLine + "\tBuilding Init DF...")
+    list_of_dfs.append(pd.read_csv(curFile, compression='gzip', header=0, sep=',', quotechar='"'))
 
-    #Convert coordinate to loc_x and loc_y
-    #place_data.coordinate.str.split(',',expand=True).rename(columns={0:'loc_x', 1:'loc_y'})
+    print(preLine + "\tExplode Coordinates...")
+    #DropNA to remove 2nd file's extra columns (one row has data there? Wonder what it is.)
+    list_of_dfs[-1][['loc_x', 'loc_y']] = list_of_dfs[-1]['coordinate'] \
+                                            .str.split(',', expand=True) \
+                                            .dropna(axis='columns')
 
-    #df = pd.concat([place_data, place_data.coordinate.str.split(',',expand=True)\
-    #                            .rename(columns={0:'loc_x', 1:'loc_y'})])
-
-    #place_data['loc_x'] = place_data.coordinate.str.split(",", expand=True)\
-    #                            .rename(columns={0:'loc_x', 1:'loc_y'})['loc_x']
-    #
-    #place_data['loc_y'] = place_data.coordinate.str.split(",", expand=True)\
-    #                            .rename(columns={0:'loc_x', 1:'loc_y'})['loc_y']
-
-    print(curFile + "\tExplode Coordinates...")
-    place_data[['loc_x', 'loc_y']] = place_data['coordinate'].str.split(',', expand=True)
-
-    print(curFile + "\tCategorize user_id...")
+    print(preLine + "\tCategorize user_id...")
     #TODO: Figure out how to convert user_id to number
     #df['ssn_anon'] = df['ssn'].astype('category').cat.codes
-    place_data['user_id_cat'] = place_data['user_id'].astype('category').cat.codes
+    list_of_dfs[-1]['user_id_cat'] = list_of_dfs[-1]['user_id'] \
+                                            .astype('category').cat.codes
 
-    print(curFile + "\tDropping un-necessary columns...")
-    place_data = place_data.drop(columns=['coordinate', 'user_id'])
+    print(preLine + "\tDropping un-necessary columns...")
+    list_of_dfs[-1] = list_of_dfs[-1].drop(columns=['coordinate', 'user_id'])
 
-    print(place_data.head())
-    print(place_data.columns)
-    print(place_data.info())
+    total_size+=len(list_of_dfs[-1])
+
+    print(preLine + "\tRows: " + f'{len(list_of_dfs[-1]):,}')
+    print(preLine + "\tTotal Size: " + f'{total_size:,}')
+    #print(place_data.head())
+    #print(place_data.columns)
+    #print(place_data.info())
+
+combined_df = pd.concat(list_of_dfs, ignore_index=True)
+combined_df.to_pickle(path="res/data/dataframe.pkl")
